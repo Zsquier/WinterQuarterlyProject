76b514c4fa500806b5dbb9a2e434982cc1d722c3
Chris Dunn <crdunn@cpp.edu>
2018-02-01 12:48:37 -0800

    randomizeBoard and updateBoard +some

--- Software/Sketches/sketch_game_of_life/sketch_game_of_life.ino
+++ Software/Sketches/sketch_game_of_life/sketch_game_of_life.ino
@@ -27,8 +27,8 @@
  * 
  * https://github.com/Zsquier/WinterQuarterlyProject
  */
-int PROGMEM board[][];
-int PROGMEM pins[];
+byte PROGMEM board[][];
+byte PROGMEM pins[];
 
 // code to run once
 void setup() {
@@ -50,13 +50,18 @@ void loop() {
  * Function takes input x and y as coordinates of cell that needs to be checked and returns the amount of neighbors that the cell has
  * Notes: Don't check cells that are out of bounds. Can return once neighbors is 4 since greater then 4 doesnt effect state.
  */
-int getNeighbors(int x, int y){
-  int neighbors = 0;   //amount of neighbors around a cell
-  for (int a = y-1; a <= y+1; a++){
-    for (int b = x-1; b <= x+1; b++){
-      if((a == y && b == x) || (a < 0) || (a > 31) || (b < 0) || (b > 31)) continue;    //This makes sure not to increase neighbors if it is itself or out or bounds.
-      if(board[a][b] == 1) neighbors++;         //when a neighbor is alive increase neighbors by one
-      if(neighbors == 4) return neighbors;      //It doesn't matter if there are more then 4 neighbors around a cell so we can return at 4
+byte getNeighbors(byte x, byte y){
+  byte neighbors = 0;   //amount of neighbors around a cell
+  for (byte a = y-1; a <= y+1; a++){
+    for (byte b = x-1; b <= x+1; b++){
+      if((a >= 0) && (a <= 31) && (b >= 0) && (b <= 31)){
+        if((a == y && b == x)) 
+          continue;    //This makes sure not to increase neighbors if it is itself or out or bounds.
+        if(board[a][b] == 1)
+          neighbors++;         //when a neighbor is alive increase neighbors by one
+        if(neighbors > 3) 
+          return neighbors;      //It doesn't matter if there are more then 4 neighbors around a cell so we can return at 4
+      }
     }
   }
   return neighbors;
@@ -67,9 +72,65 @@ int getNeighbors(int x, int y){
  * Purpose: Clears the board to all zero/dead
  */
 void clearBoard(){
-  for(x=0;x<32;x++){
-    for(y=0;y<32;y++){
-      board[x][y]==0;
+  for(byte x=0;x<32;x++){
+    for(byte y=0;y<32;y++){
+      board[x][y]=0;
     }
   }
 }
+
+/*
+ * Randomize Board
+ * Purpose: Set all cells to either 0 or 1
+ */
+ void ranodmizeBoard(){
+  for(byte x=0;x<32;x++){
+    for(byte y=0;y<32;y++){
+      board[x][y]=random(0,2);
+    }
+  }
+ }
+
+ /*
+  * Update Board
+  * Purpose: Update the gameBoard to new state
+  * TO DO: Try to make pointers work
+  */
+void updateBoard(){
+  byte boardNew[32][32];
+  byte neighbors = 0;
+  for(byte x=0;x<32;x++){
+    for(byte y=0;y<32;y++){
+      neighbors = getNeighbors(x,y);
+      //rule 1.
+      if(neighbors < 2 && board[x][y]==1)
+        boardNew[x][y] = 0;
+      //rule 2.
+      else if((neighbors == 2 || neighbors == 3) && board[x][y]==1 )
+        boardNew[x][y] = 1;
+      //rule 3.
+      else if( neighbors > 3 && board[x][y]==1)
+        boardNew[x][y] = 0;
+      //rule 4.
+      else if(neighbors == 3 && board[x][y]==0)
+        boardNew[x][y] = 1;
+      //Set boardNew dead needs to stay dead
+      else
+        boardNew[x][y] = 0;
+    }
+  }
+  for(x=0;x<32;x++){
+    for(y=0;y<32;y++){
+      board[x][y] = boardNew[x][y];
+    }
+  }
+}
+/*
+ * Determrine Equilibrium
+ * Find average values to detect when board stops changing. Check when the board enters a state of equilibrium
+ */
+void determineEquilibrium(byte eSize, int counter){
+  float Equilibrium[eSize];
+  
+}
+
